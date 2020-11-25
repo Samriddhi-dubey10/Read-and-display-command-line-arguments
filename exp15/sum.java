2ce6bf88ba17bbe0101846a9696b9785afc9ebd6
Samriddhi-dubey10 <74581936+Samriddhi-dubey10@users.noreply.github.com>
2020-11-25 20:02:24 +0530

    Add files via upload

---
+++ exp15/sum.java
@@ -0,0 +1,13 @@
+package exp15;
+
+public class sum {
+	public static void main(String ar[]) {
+		int a,b,sum;
+		a=Integer .parseInt(ar[0]);
+		b=Integer.parseInt(ar[1]);
+		
+		sum = a+b;
+		System.out.println("Sum of " +a+ " and "+b+" is " +sum);
+	}
+
+}
